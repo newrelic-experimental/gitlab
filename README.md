da27aa5e82ba03fd04628728a00865246dda790e
dpascheconr <dpacheco@newrelic.com>
2022-11-21 14:17:16 +0000

    com
    com
    
    com
    
    com
    
    com
    
    com
    
    com
    
    com
    
    com
    
    com
    
    com
    
    com
    
    com
    
    com
    
    com
    
    com
    
    com
    
    com
    
    com
    
    com
    
    feat(gitlab): Gitlab monitoring
    
    Gitlab Monitoring
    
    Gitlab Monitoring

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
-# gitlab
+
+#New Relic Metrics Exporter
+
+
+[![New Relic Experimental header](https://github.com/newrelic/opensource-website/raw/master/src/images/categories/Experimental.png)](https://opensource.newrelic.com/oss-category/#new-relic-experimental)
+
+# New Relic Gitlab Exporters
+>Monitor Gitlab with OpenTelemetry and New Relic quickstarts
+
+![GitHub forks](https://img.shields.io/github/forks/newrelic-experimental/tls-proxy?style=social)
+![GitHub stars](https://img.shields.io/github/stars/newrelic-experimental/tls-proxy?style=social)
+![GitHub watchers](https://img.shields.io/github/watchers/newrelic-experimental/tls-proxy?style=social)
+
+![GitHub last commit](https://img.shields.io/github/last-commit/newrelic-experimental/tls-proxy)
+![GitHub issues](https://img.shields.io/github/issues/newrelic-experimental/tls-proxy)
+![GitHub pull requests](https://img.shields.io/github/issues-pr/newrelic-experimental/tls-proxy)
+
+
+## How to monitor Gitlab with New Relic
+
+Now, you can monitor your Gitlab data with New Relic using New Relic Exporter and Metrics Exporter, making it easier to get observability into your CI/CD pipeline health and performance.
+
+Using these exporters you will be able to:
+
+- Visualise key metrics on your Gitlab pipelines, such as how long your jobs are taking, how often they are failing
+- Visualise jobs and pipeline executions as distributed traces with logs in context 
+- Pinpoint where issues are coming from in your pipelines.
+- Create alerts on your Gitlab pipelines.
+
+The next image shows a New Relic dashboard with some of the Gitlab metrics you’ll be able to visualise.
+
+
+## Gitlab Dashboard
+
+![Gitlab Dashboard](screenshots/gitlab_dashboard.jpg)
+
+
+# New Relic Exporter
+
+
+| Variables | Description | Optional | Values | Default |
+| ---       |         --- |       ---| ---    |   ----   |
+| `OTEL_EXPORTER_OTEL_ENDPOINT` | New Relic OTEL endpoint including port | True | String | "https://otlp.nr-data.net:4318" or "https://otlp.eu01.nr-data.net:4318" |
+| `GLAB_TOKEN` | Token to access gitlab API | False | String | None |
+| `NEW_RELIC_API_KEY` | New Relic License Key | False | String | None |
+| `GLAB_SERVICE_NAME` | New Relic OTEL entity name | True | String | True |
+| `GLAB_EXPORT_LOGS` | Export job logs to New Relic | True | Boolean | True |
+
+
+# New Relic Metrics Exporter
+
+| Variables | Description | Optional | Values | Default |
+| ---       |         --- |       ---| ---    |   ----   |
+| `OTEL_EXPORTER_OTEL_ENDPOINT` | New Relic OTEL endpoint including port | True | String | "https://otlp.nr-data.net:4318" or "https://otlp.eu01.nr-data.net:4318" |
+| `GLAB_TOKEN` | Token to access gitlab API | False | String | None |
+| `NEW_RELIC_API_KEY` | New Relic License Key | False | String | None |
+| `GLAB_SERVICE_NAME` | New Relic OTEL entity name | True | String | Project name |
+| `GLAB_EXPORT_GROUPS_REGEX` | Regex to match group names against | False | Boolean | None |
+| `GLAB_EXPORT_PROJECTS_REGEX` | Regex to match project names against | False | Boolean | None |
+| `GLAB_EXPORT_NON_GROUP_PROJECTS` | Enable if we should export non group projects, i.e. user projects | True | Boolean | False |
+| `GLAB_EXPORT_LAST_MINUTES` | The amount past minutes to export data from | True | Integer | 60 |
+| `GLAB_ENVS_DROP` | Extra system environment variables to drop from span attributes | True | List* | NEW_RELIC_API_KEY,GITLAB_FEATURES,CI_SERVER_TLS_CA_FILE,CI_RUNNER_TAGS,CI_JOB_JWT,CI_JOB_JWT_V1,CI_JOB_JWT_V2,GLAB_TOKEN,GIT_ASKPASS,CI_COMMIT_BEFORE_SHA,CI_BUILD_TOKEN,CI_DEPENDENCY_PROXY_PASSWORD,CI_RUNNER_SHORT_TOKEN,CI_BUILD_BEFORE_SHA,CI_BEFORE_SHA,OTEL_EXPORTER_OTEL_ENDPOINT,GLAB_DIMENSION_METRICS |
+| `GLAB_ATTRIBUTES_DROP` | Attributes to drop from logs and spans events | True | List* | None |
+| `GLAB_DIMENSION_METRICS` | Extra dimensional metric attributes to add to each metric | True | List* | status,stage,name |
+| `GLAB_STANDALONE` | Set to True if not running as gitlab pipeline schedule | True | Boolean | False |
+*comma separated
+
+
+## Contributing
+
+We encourage your contributions to improve [Gitlab Exporters](../../)! Keep in mind when you submit your pull request, you'll need to sign the CLA via the click-through using CLA-Assistant. You only have to sign the CLA one time per project. If you have any questions, or to execute our corporate CLA, required if your contribution is on behalf of a company, please drop us an email at opensource@newrelic.com.
+
+**A note about vulnerabilities**
+
+As noted in our [security policy](../../security/policy), New Relic is committed to the privacy and security of our customers and their data. We believe that providing coordinated disclosure by security researchers and engaging with the security community are important means to achieve our security goals.
+
+If you believe you have found a security vulnerability in this project or any of New Relic's products or websites, we welcome and greatly appreciate you reporting it to New Relic through [HackerOne](https://hackerone.com/newrelic).
+
+## License
+
+Gitlab Exporters are licensed under the [Apache 2.0](http://apache.org/licenses/LICENSE-2.0.txt) License.
+
+>Gitlab Exporters also use source code from third-party libraries. You can find full details on which libraries are used and the terms under which they are licensed in the third-party notices document.]
+
---
+++ new-relic-exporter/exporter.py
@@ -0,0 +1,197 @@
+import json
+import logging
+import os
+from datetime import datetime, timedelta
+import pytz
+from parser import (check_env_vars, do_parse, do_string, do_time,
+                    grab_span_att_vars, parse_attributes)
+
+import gitlab
+from opentelemetry import trace
+from opentelemetry.instrumentation.logging import LoggingInstrumentor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.trace import Status, StatusCode
+from otel import create_resource_attributes, get_logger, get_tracer
+
+
+GLAB_EXPORT_LOGS = True
+
+def send_to_nr():
+    check_env_vars(metrics=False)
+
+    # Set variables
+    global GLAB_EXPORT_LOGS 
+    GLAB_TOKEN = os.getenv('GLAB_TOKEN')
+    NEW_RELIC_API_KEY = os.getenv('NEW_RELIC_API_KEY')
+    project_id = os.getenv('CI_PROJECT_ID')
+    pipeline_id = os.getenv('CI_PARENT_PIPELINE')
+    
+    if "OTEL_EXPORTER_OTEL_ENDPOINT" in os.environ:
+        OTEL_EXPORTER_OTEL_ENDPOINT = os.getenv('OTEL_EXPORTER_OTEL_ENDPOINT')
+    else: 
+        if NEW_RELIC_API_KEY.startswith("eu"):
+            OTEL_EXPORTER_OTEL_ENDPOINT = "https://otlp.eu01.nr-data.net:4318"
+        else:
+            OTEL_EXPORTER_OTEL_ENDPOINT = "https://otlp.nr-data.net:4318"
+        
+    if "GLAB_EXPORT_LOGS" in os.environ:
+        GLAB_EXPORT_LOGS = os.getenv('GLAB_EXPORT_LOGS')
+        if GLAB_EXPORT_LOGS.lower() == "false":
+            GLAB_EXPORT_LOGS = False
+
+    # Set gitlab client
+    gl = gitlab.Gitlab(private_token="{}".format(GLAB_TOKEN))
+
+    # Set gitlab project/pipeline/jobs details
+    project = gl.projects.get(project_id)
+    pipeline = project.pipelines.get(pipeline_id)
+    project_full_name = str((project.attributes.get('name_with_namespace'))).lower().replace(" ", "")
+    GLAB_SERVICE_NAME = project_full_name
+
+    jobs = pipeline.jobs.list()
+    job_lst=[]
+    for job in jobs:
+        job_json = json.loads(job.to_json())
+        if (job_json['name']) not in ["new-relic-exporter", "new-relic-metrics-exporter"]:
+            job_lst.append(job_json)
+
+    #Set variables to use for OTEL metrics and logs exporters
+    global_resource = Resource(attributes={
+    SERVICE_NAME: GLAB_SERVICE_NAME,
+    "pipeline_id": str(os.getenv('CI_PARENT_PIPELINE')),
+    "project_id": str(os.getenv('CI_PROJECT_ID')),
+    "gitlab.source": "gitlab-exporter",
+    "gitlab.resource.type": "span"
+    })
+    endpoint="{}".format(OTEL_EXPORTER_OTEL_ENDPOINT)
+    headers="api-key={}".format(NEW_RELIC_API_KEY)
+    LoggingInstrumentor().instrument(set_logging_format=True)
+    logging.basicConfig(filename="exporter.log")
+
+    #Create global tracer to export traces to NR
+    tracer = get_tracer(endpoint, headers, global_resource, "tracer")
+    
+    #Configure env variables as span attributes
+    atts = grab_span_att_vars()
+    
+    #Configure spans
+    pipeline_att = json.loads(pipeline.to_json())
+    # Create a new root span(use start_span to manually end span with timestamp)
+    p_parent = tracer.start_span(name=GLAB_SERVICE_NAME + " - pipeline: "+os.getenv('CI_PARENT_PIPELINE'), attributes=atts, start_time=do_time(str(pipeline_att['started_at'])), kind=trace.SpanKind.SERVER)
+    try:
+        for attribute in pipeline_att:
+            if type(attribute) is dict:
+                for sub_att in pipeline_att[attribute]:
+                    p_parent.set_attribute(do_string(attribute)+"."+str(sub_att),str(pipeline_att[attribute][sub_att]))
+            else:
+                p_parent.set_attribute(do_string(attribute),str(pipeline_att[attribute]))
+
+        if pipeline_att['status'] == "failed":
+            p_parent.set_status(Status(StatusCode.ERROR,"Pipeline failed, check jobs for more details")) 
+
+        #Set the current span in context(parent)
+        pcontext = trace.set_span_in_context(p_parent)
+        for job in job_lst:
+            #Set job level tracer and logger
+            job_attributes = parse_attributes(job)
+            resource_attributes ={SERVICE_NAME: GLAB_SERVICE_NAME,"pipeline_id": str(os.getenv('CI_PARENT_PIPELINE')),"project_id": str(os.getenv('CI_PROJECT_ID')),"job_id": str(job["id"]),"gitlab.source": "gitlab-exporter","gitlab.resource.type": "span"}
+            resource_attributes.update(create_resource_attributes(job_attributes,GLAB_SERVICE_NAME ))
+            resource_log = Resource(attributes=resource_attributes)
+            job_tracer = get_tracer(endpoint, headers, resource_log, "job_tracer")
+            try:
+                if (job['status']) == "skipped":
+                    # Create a new child span for every valid job, set it as the current span in context
+                    child = job_tracer.start_span(name="Stage: " + str(job['name'])+" - job_id: "+ str(job['id']) + "- SKIPPED",context=pcontext,kind=trace.SpanKind.CONSUMER)
+                    child.end()
+                else:
+                    # Create a new child span for every valid job, set it as the current span in context
+                    child = job_tracer.start_span(name="Stage: " + str(job['name'])+" - job_id: "+ str(job['id']), start_time=do_time(job['started_at']),context=pcontext, kind=trace.SpanKind.CONSUMER)
+                    with trace.use_span(child, end_on_exit=False):
+                        try:
+                            if job['status'] == "failed":
+                                current_job = project.jobs.get(job['id'], lazy=True)
+                                with open("job.log", "wb") as f:
+                                    current_job.trace(streamed=True, action=f.write)
+
+                                with open("job.log", "rb") as f:
+                                    log_data = ""
+                                    for string in f:
+                                        log_data+=str(string.decode('utf-8', 'ignore'))
+                                
+                                match = log_data.split("ERROR: Job failed: ")
+                                if do_parse(match):
+                                    child.set_status(Status(StatusCode.ERROR,str(match[1])))
+                                else:
+                                    child.set_status(Status(StatusCode.ERROR,str(job['failure_reason'])))
+                            child.set_attributes(parse_attributes(job))
+
+                            if GLAB_EXPORT_LOGS:
+                                try:
+                                    if job['status'] == "failed":
+                                        pass
+                                    else:
+                                        with open("job.log", "wb") as f:
+                                            current_job = project.jobs.get(job['id'], lazy=True)
+                                            current_job.trace(streamed=True, action=f.write)
+                                    with open("job.log", "rb") as f:  
+                                        err = False
+                                        for string in f:
+                                            if string.decode('utf-8').__contains__('ERROR'):
+                                                err = True
+                                                
+                                    with open("job.log", "rb") as f:
+                                        resource_attributes_base ={SERVICE_NAME: GLAB_SERVICE_NAME,"pipeline_id": str(os.getenv('CI_PARENT_PIPELINE')),"project_id": str(os.getenv('CI_PROJECT_ID')),"job_id": str(job["id"]),"gitlab.source": "gitlab-exporter","gitlab.resource.type": "span"}
+                                        if err:
+                                            count = 1
+                                            for string in f:
+                                                if string.decode('utf-8') != "\n":
+                                                    if count == 1:
+                                                        resource_attributes["message"] = string.decode('utf-8')
+                                                        resource_attributes.update(resource_attributes_base)
+                                                        resource_log = Resource(attributes=resource_attributes)
+                                                        job_logger = get_logger(endpoint,headers,resource_log, "job_logger")
+                                                        job_logger.error("")
+                                                    else:
+                                                        resource_attributes_base["message"] = string.decode('utf-8')
+                                                        resource_log = Resource(attributes=resource_attributes_base)
+                                                        job_logger = get_logger(endpoint,headers,resource_log, "job_logger")
+                                                        job_logger.error("")
+                                                    count += 1
+                                        else: 
+                                            count = 1
+                                            for string in f:
+                                                if count == 1:
+                                                    if string.decode('utf-8') != "\n":
+                                                        resource_attributes["message"] = string.decode('utf-8')
+                                                        resource_log = Resource(attributes=resource_attributes)
+                                                        job_logger = get_logger(endpoint,headers,resource_log, "job_logger")
+                                                        job_logger.info("")
+                                                else:
+                                                    if string.decode('utf-8') != "\n":
+                                                        resource_attributes_base["message"] = string.decode('utf-8')
+                                                        resource_log = Resource(attributes=resource_attributes_base)
+                                                        job_logger = get_logger(endpoint,headers,resource_log, "job_logger")
+                                                        job_logger.info("")
+                                                count += 1
+
+                                except Exception as e:
+                                    print(e)
+                            else:
+                                print("Not configured to send logs New Relic, skip...")    
+
+                        finally:
+                            child.end(end_time=do_time(job['finished_at']))
+
+                if job == (len(job_lst)-1):
+                    print(job)
+
+            except Exception as e:
+                print(e)      
+
+        
+        print("All data sent to New Relic for pipeline: " + str(pipeline_att['id']))
+        print("Terminating...")
+
+    finally:
+        p_parent.end(end_time=do_time(str(pipeline_att['finished_at'])))
+send_to_nr()
